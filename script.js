075bd678bcaafab4cc78358d41a948261af769ab
Alexis Arriola Martinez <20253ds138@utez.edu.mx>
2026-06-01 14:09:43 -0600

    [NEW] feature/proyecto perfil

---
+++ script.js
@@ -0,0 +1,40 @@
+const boton = document.getElementById("btn-saludo");
+let saludoMostrado = false;
+
+boton.addEventListener("click", function() {
+    alert("Hola, gracias por visitar mi perfil!");
+});
+
+
+boton.addEventListener("click", function() {
+    const seccionContacto = document.getElementById("contacto");
+
+    if (!saludoMostrado) {
+        const mensaje = document.createElement("p");
+        mensaje.textContent = "Gracias por visitar mi perfil. Espero que volvamos a vernos!";
+        mensaje.style.color = "#27ae60";
+        mensaje.style.fontWeight = "bold";
+        seccionContacto.appendChild(mensaje);
+
+        boton.textContent = "Saludo enviado";
+        boton.disabled = true;
+        boton.style.backgroundColor = "#7f8c8d";
+
+        saludoMostrado = true;
+    }
+});
+
+const boton2 = document.getElementById("btn-nombre");
+let nombreMostrado = false;
+
+boton2.addEventListener("click", function() {
+    const seccionProyecto= document.getElementById("proyectos");
+
+if (!nombreMostrado) {
+    const botonTexto = document.querySelector( "h1");
+    botonTexto.textContent = "Alexis Arriola Martinez";
+    bottonTexto.style.color = "#aeac27";
+    boton2.style.fontWeight = "bold";
+
+}
+})
